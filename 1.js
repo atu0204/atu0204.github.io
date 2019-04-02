69b1e78c61f725426bc485f8b9c91509a2a4ff36
atu0204 <chautu0204@gmail.com>
2019-04-02 18:31:17 +0700

    update cmt facebook

--- 1.js
+++ 1.js
@@ -1,4 +1,9 @@
 ﻿ $(function(){
- 
+    vitriedu =$('#edu').offset().top;
+    console.log(vitriedu);
+
+    $('.hr').click(function(){
+        $('body,html').animate({scrollTop:vitriedu},500)
+    })
 })  
  
